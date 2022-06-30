0518af45ab3434adf61603d00f6e8ccd1e89bf2d
Yirga Mulaw <infoyirga12@gmail.com>
2022-06-30 18:16:24 +0000

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-/** 
- * main - Execussion starts here
- * Return: zero
- */
+/**
+* main - Execution starts here
+* Return: zero
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
