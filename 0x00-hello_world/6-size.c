714c3ffee3490a49388e8da81441a77f79e5d224
Yirga Mulaw <infoyirga12@gmail.com>
2022-06-30 16:25:56 +0000

    Printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-/* 
+/** 
  * main - Entry point
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
