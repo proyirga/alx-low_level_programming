46319fca8031ad63672df74b7482a003c7c32b06
Yirga Mulaw <infoyirga12@gmail.com>
2022-07-01 19:36:41 +0000

    Comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - Execution starts here
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, z;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			for (z = j + 1; z <= '9'; z++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(z);
+				if (i != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
