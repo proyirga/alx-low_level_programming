355955c39a3b943c746b8f624bb54d50c1e5eccf
Yirga Mulaw <infoyirga12@gmail.com>
2022-07-01 17:52:06 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Strating point
+ * Return: - always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
