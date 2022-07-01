6eb5c3d4cc2c30f9dac1bd287c93867ae6e8004a
Yirga Mulaw <infoyirga12@gmail.com>
2022-07-01 18:35:38 +0000

    Combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Execution starts here
+ * Return: 0
+ */
+int main(void)
+{
+	char al;
+
+	al = '0';
+
+	do {
+		putchar(al);
+		if (al < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		al++;
+	} while (al <= '9');
+	putchar('\n');
+	return (0);
+}
