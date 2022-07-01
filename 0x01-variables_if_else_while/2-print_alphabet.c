7125742316ac638448336cf340f01b606546afde
Yirga Mulaw <infoyirga12@gmail.com>
2022-07-01 16:46:27 +0000

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Execution starts here
+ * Return: 0
+ */
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}
