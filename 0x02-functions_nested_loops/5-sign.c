744ca425f402964767400169ef11a3b54eaf935b
Yirga Mulaw <infoyirga12@gmail.com>
2022-07-05 17:37:28 +0000

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - checks for sign +, - or 0
+ * @n: given input
+ * Return: 1 if n is greater than 0, 0 if n is 0 or -1 if n is less than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (-1);
+	}
+	_putchar('\n');
+}
