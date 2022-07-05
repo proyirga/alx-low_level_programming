b85f6b8fd7d1f1cf5f35ad9b070048b5e8ae6176
Yirga Mulaw <infoyirga12@gmail.com>
2022-07-05 16:47:20 +0000

    Alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -print 10 times alphabets a - z
+ * Return: no return value
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
