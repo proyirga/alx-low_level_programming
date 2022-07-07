57191c8f4799d78f05acce47288894ee464e20d4
Yirga Mulaw <infoyirga12@gmail.com>
2022-07-07 20:54:24 +0000

    Functions and loops: 9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int n = 1;
+
+	while (n < 100)
+	{
+		if (n % 15 == 0)
+			printf("FizzBuzz ");
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", n);
+		n++;
+	}
+	printf("Buzz\n");
+	return (0);
+}
