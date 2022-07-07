fabeab7182f812baf99c39d70d05afd3155348da
Yirga Mulaw <infoyirga12@gmail.com>
2022-07-07 19:14:42 +0000

    Functions and loops: 2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - returns multiplication of two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: returns multiples of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,8 @@ int _isupper(int c);
 
 int _isdigit(int c);
 
+int mul(int a, int b);
+
 void positive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
