b04b7e0a550064f2bfa8df62fa7dfb7a94f45483
Yirga Mulaw <infoyirga12@gmail.com>
2022-07-28 19:29:09 +0000

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * @mul: result
+ * @num1: first number
+ * @num2: second number
+ *
+ * Return: muktiplication of two nums
+ */
+
+
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: elements of an array
+ * @size: bytes of each element
+ *
+ * Return: returns a pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		arr[i] = 0;
+	return (arr);
+}
