2d661394af19d9d15715ddff84eafc9f645280ad
Yirga Mulaw <infoyirga12@gmail.com>
2022-11-18 15:17:11 +0300

    Printing chars using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 on success
+ *
+ * Description: this program prints
+ * +out a string to stdou using puts()
+ *
+ */
+
+int main(void)
+{
+	char s[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(s);
+	return (0);
+}
