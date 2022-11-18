03b657d8a7533c998355347f1894421ebd0db888
Yirga Mulaw <infoyirga12@gmail.com>
2022-11-18 15:28:24 +0300

    Printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description:
+ * C program that prints exactly
+ * +\"with proper grammar, but the outcome is a piece of art,\"
+ * +followed by a new line.
+ *     Use the function printf
+ *     You are not allowed to use the function puts
+ *     Your program should return 0
+ *     Your program should compile without warning when using
+ *     +the -Wall gcc option
+ * Return: 0 on success
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
