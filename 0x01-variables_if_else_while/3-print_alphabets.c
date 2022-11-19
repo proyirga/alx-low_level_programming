d7d95e7813dfbea96fefebc9bf6a16fbabf59845
Yirga Mulaw <infoyirga12@gmail.com>
2022-11-19 15:01:54 +0300

    AlPHA

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * mani - Entry point
+ * Return: 0 on success
+ *
+ * Description: Program that prints the alphabet in lowercase, and then
+ * +in uppercase, followed by a new line.
+ * Only use the putchar function
+ *	Code should be in the main function
+ *	Only use putchar three times in your code
+ */
+
+int main(void)
+{
+	char c, cc;
+
+	c = 'a';
+	cc = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (cc <= 'Z')
+	{
+		putchar(cc);
+		cc++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
